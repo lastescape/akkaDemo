4a261bf0163fc0f01974fab29a54e267826ef413
qdh <bust_nakoruru@163.com>
2018-05-29 20:32:48 +0800

    GC

--- src/main/scala/com/qdh/demo/MockServer.scala
+++ src/main/scala/com/qdh/demo/MockServer.scala
@@ -10,6 +10,9 @@ import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
 import spray.json.DefaultJsonProtocol._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import com.alibaba.fastjson.{JSON, JSONObject}
+import org.eclipse.jetty.client.HttpClient
+import org.eclipse.jetty.util.ssl.SslContextFactory
 
 import scala.collection.mutable
 import scala.io.StdIn
@@ -31,6 +34,39 @@ object MockServer {
 
     final case class TempClass(input_1: String, input_2: String)
     implicit val tempClassFormat = jsonFormat2(TempClass)
+
+    final case class TaskStatusInfo(schedule_id: String,
+                                    schedule_time: String,
+                                    name: String,
+                                    guid: String,
+                                    schedule_time_offset: String,
+                                    start_time: String,
+                                    stop_time: String,
+                                    status: String,
+                                    today: String,
+                                    gmt_date: String)
+
+    implicit val TaskStatusInfoFormat = jsonFormat10(TaskStatusInfo)
+    final case class TaskStatusList(code: Int, data: Array[TaskStatusInfo])
+    implicit val TaskStatusListFormat = jsonFormat2(TaskStatusList)
+
+
+    final case class TaskCodeInfoItem(id: String,
+                                  name: String,
+                                  description: String,
+                                  `type`: String,
+                                  owner: String,
+                                  host: String,
+                                  schedule_mode: String,
+                                  schedule_interval: String,
+                                  schedule_day: String,
+                                  sequential: String,
+                                  delay_execution: String,
+                                  create_time: String
+                                 )
+    final case class TaskCodeInfo(code: Int, data: Array[TaskCodeInfoItem])
+
+
 
     private val userGroup = mutable.ListBuffer[User]()
 
@@ -65,7 +101,35 @@ object MockServer {
                     entity(as[TempClass]) {
                         fieldMap => {
                             extractClientIP { ip =>
-                              complete(fieldMap)
+
+                                // Instantiate and configure the SslContextFactory
+                                val sslContextFactory = new SslContextFactory()
+
+                                // Instantiate HttpClient with the SslContextFactory
+                                val httpClient = new HttpClient(sslContextFactory)
+
+                                // Configure HttpClient, for example:
+                                httpClient.setFollowRedirects(false)
+
+                                // Start HttpClient
+                                httpClient.start()
+
+
+                                // get task code
+                                val code_response = httpClient.GET(s"http://10.40.8.115:8080/api/v1/task_search?name_seg=${fieldMap.input_1}")
+                                val code_responseContent = code_response.getContentAsString()
+                                val task_code = JSON.parseObject[TaskCodeInfo](code_responseContent, classOf[TaskCodeInfo])
+
+                                if (task_code != null) {
+                                    val code = task_code.data(0).id
+                                    val response = httpClient.GET(s"http://10.40.8.115:8080/api/v1/task_executions/${code}")
+                                    val responseContent = response.getContentAsString()
+                                    val taskStatusList = JSON.parseObject[TaskStatusList](responseContent, classOf[TaskStatusList])
+                                    complete(taskStatusList)
+                                } else {
+                                    complete("")
+                                }
+
                             }
                         }
                     }
